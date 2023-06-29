128baf4f68017d523d4063f3199aad506e7db92d
MeekoDev <DiegoCru1024@hotmail.com>
2023-06-29 12:58:58 -0500

    Update mainPage.js

--- cliente/src/components/mainPage.js
+++ cliente/src/components/mainPage.js
@@ -82,7 +82,7 @@ export default function MainPage() {
         const formData = new FormData()
         formData.append("frame", blob, "frame.jpg")
         axios
-            .post("http://localhost:5000/api/sendData", formData)
+            .post("http://localhost:8080/api/sendData", formData)
             .then((response) => {
                 setResponse(response.data)
             })
